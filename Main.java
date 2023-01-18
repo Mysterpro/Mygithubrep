e6b9a29846c2f9b54bd119ee58eb06598264822c
Mysterpro <123026806+Mysterpro@users.noreply.github.com>
2023-01-18 22:38:16 +0100

    Mycommit
    First commit of rep

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 141;
+        int b = 155;
+
+        int c = a + b;
+        System.out.println("Сложение а и б составляет = " + c);
+
+    }
+
+}
